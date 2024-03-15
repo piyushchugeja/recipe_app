9151da74c2deb31c328bd2e7dbd54f05aef1662e
piyushchugeja <piyushchugeja@gmail.com>
2024-03-16 02:28:11 +0530

    add firebase support

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:recipe_app/widgets/recipe_card.dart';
 
@@ -9,95 +10,96 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
-  final recipes = [
-    {
-      "title": "Pasta",
-      "imageUrl": "images/pasta.jpg",
-      "calories": "90",
-      "cookTime": "20 min",
-      "cuisine": "Italian",
-      "ingredients": [
-        "200g pasta",
-        "Salt to taste",
-        "2 tbsp olive oil",
-        "2 cloves garlic, minced",
-        "Your favorite pasta sauce",
-        "Grated cheese and herbs for garnish"
-      ],
-      "instructions": [
-        "Boil water in a pot and add salt.",
-        "Add pasta and cook for the instructed time.",
-        "Drain the pasta and set aside.",
-        "In a separate pan, heat olive oil and add minced garlic.",
-        "Add cooked pasta to the pan and toss with desired sauce.",
-        "Serve hot and garnish with grated cheese and herbs."
-      ]
-    },
-    {
-      "title": "Pizza",
-      "imageUrl": "images/pizza.jpg",
-      "calories": "120",
-      "cookTime": "30 min",
-      "cuisine": "Italian",
-      "ingredients": [
-        "Pizza dough",
-        "Tomato sauce",
-        "Your favorite pizza toppings (cheese, vegetables, meat)",
-        "Flour for dusting"
-      ],
-      "instructions": [
-        "Preheat your oven to the specified temperature.",
-        "Roll out pizza dough and place it on a baking sheet.",
-        "Spread tomato sauce evenly over the dough.",
-        "Add your favorite toppings such as cheese, vegetables, and meat.",
-        "Bake the pizza in the preheated oven until the crust is golden and the cheese is bubbly.",
-        "Slice and serve hot."
-      ]
-    },
-    {
-      "title": "Burger",
-      "imageUrl": "images/burger.jpg",
-      "calories": "150",
-      "cookTime": "25 min",
-      "cuisine": "American",
-      "ingredients": [
-        "Ground beef",
-        "Salt and pepper to taste",
-        "Burger buns",
-        "Lettuce, tomato, onion for toppings"
-      ],
-      "instructions": [
-        "Preheat a grill or skillet over medium-high heat.",
-        "Season ground beef with salt and pepper and shape into patties.",
-        "Cook the patties for about 3-4 minutes on each side, or until desired doneness.",
-        "Toast burger buns on the grill until lightly browned.",
-        "Assemble the burgers with lettuce, tomato, onion, and any other desired toppings.",
-        "Serve hot with fries or salad."
-      ]
-    },
-    {
-      "title": "Sushi",
-      "imageUrl": "images/sushi.jpg",
-      "calories": "100",
-      "cookTime": "40 min",
-      "cuisine": "Japanese",
-      "ingredients": [
-        "Sushi rice",
-        "Nori sheets",
-        "Your choice of sushi fillings (fish, vegetables, avocado)",
-        "Soy sauce and wasabi for serving"
-      ],
-      "instructions": [
-        "Cook sushi rice according to package instructions and let it cool.",
-        "Place a sheet of nori on a bamboo sushi mat.",
-        "Spread a thin layer of rice evenly over the nori, leaving a border at the top.",
-        "Add your choice of fillings such as fish, vegetables, or avocado.",
-        "Roll the sushi tightly using the bamboo mat.",
-        "Slice the sushi roll into bite-sized pieces and serve with soy sauce and wasabi."
-      ]
-    }
-  ];
+  // final recipes = [
+  //   {
+  //     "title": "Pasta",
+  //     "imageUrl": "images/pasta.jpg",
+  //     "calories": "90",
+  //     "cookTime": "20 min",
+  //     "cuisine": "Italian",
+  //     "ingredients": [
+  //       "200g pasta",
+  //       "Salt to taste",
+  //       "2 tbsp olive oil",
+  //       "2 cloves garlic, minced",
+  //       "Your favorite pasta sauce",
+  //       "Grated cheese and herbs for garnish"
+  //     ],
+  //     "instructions": [
+  //       "Boil water in a pot and add salt.",
+  //       "Add pasta and cook for the instructed time.",
+  //       "Drain the pasta and set aside.",
+  //       "In a separate pan, heat olive oil and add minced garlic.",
+  //       "Add cooked pasta to the pan and toss with desired sauce.",
+  //       "Serve hot and garnish with grated cheese and herbs."
+  //     ]
+  //   },
+  //   {
+  //     "title": "Pizza",
+  //     "imageUrl": "images/pizza.jpg",
+  //     "calories": "120",
+  //     "cookTime": "30 min",
+  //     "cuisine": "Italian",
+  //     "ingredients": [
+  //       "Pizza dough",
+  //       "Tomato sauce",
+  //       "Your favorite pizza toppings (cheese, vegetables, meat)",
+  //       "Flour for dusting"
+  //     ],
+  //     "instructions": [
+  //       "Preheat your oven to the specified temperature.",
+  //       "Roll out pizza dough and place it on a baking sheet.",
+  //       "Spread tomato sauce evenly over the dough.",
+  //       "Add your favorite toppings such as cheese, vegetables, and meat.",
+  //       "Bake the pizza in the preheated oven until the crust is golden and the cheese is bubbly.",
+  //       "Slice and serve hot."
+  //     ]
+  //   },
+  //   {
+  //     "title": "Burger",
+  //     "imageUrl": "images/burger.jpg",
+  //     "calories": "150",
+  //     "cookTime": "25 min",
+  //     "cuisine": "American",
+  //     "ingredients": [
+  //       "Ground beef",
+  //       "Salt and pepper to taste",
+  //       "Burger buns",
+  //       "Lettuce, tomato, onion for toppings"
+  //     ],
+  //     "instructions": [
+  //       "Preheat a grill or skillet over medium-high heat.",
+  //       "Season ground beef with salt and pepper and shape into patties.",
+  //       "Cook the patties for about 3-4 minutes on each side, or until desired doneness.",
+  //       "Toast burger buns on the grill until lightly browned.",
+  //       "Assemble the burgers with lettuce, tomato, onion, and any other desired toppings.",
+  //       "Serve hot with fries or salad."
+  //     ]
+  //   },
+  //   {
+  //     "title": "Sushi",
+  //     "imageUrl": "images/sushi.jpg",
+  //     "calories": "100",
+  //     "cookTime": "40 min",
+  //     "cuisine": "Japanese",
+  //     "ingredients": [
+  //       "Sushi rice",
+  //       "Nori sheets",
+  //       "Your choice of sushi fillings (fish, vegetables, avocado)",
+  //       "Soy sauce and wasabi for serving"
+  //     ],
+  //     "instructions": [
+  //       "Cook sushi rice according to package instructions and let it cool.",
+  //       "Place a sheet of nori on a bamboo sushi mat.",
+  //       "Spread a thin layer of rice evenly over the nori, leaving a border at the top.",
+  //       "Add your choice of fillings such as fish, vegetables, or avocado.",
+  //       "Roll the sushi tightly using the bamboo mat.",
+  //       "Slice the sushi roll into bite-sized pieces and serve with soy sauce and wasabi."
+  //     ]
+  //   }
+  // ];
 
+  final recipes = FirebaseFirestore.instance.collection('recipes');
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -138,17 +140,31 @@ class HomePageState extends State<HomePage> {
           ),
           const SizedBox(height: 16),
           Expanded(
-            child: ListView.builder(
-              itemCount: recipes.length,
-              itemBuilder: (context, index) {
-                return RecipeCard(
-                  title: recipes[index]['title']! as String,
-                  imageUrl: AssetImage(recipes[index]['imageUrl']! as String),
-                  calories: recipes[index]['calories']! as String,
-                  cookTime: recipes[index]['cookTime']! as String,
-                  cuisine: recipes[index]['cuisine']! as String,
-                  ingredients: recipes[index]['ingredients']! as List<String>,
-                  instructions: recipes[index]['instructions']! as List<String>,
+            // get data from firestore and display as recipecard
+            child: StreamBuilder<QuerySnapshot>(
+              stream:
+                  FirebaseFirestore.instance.collection('recipes').snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                return ListView.builder(
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (context, index) {
+                    final DocumentSnapshot recipe = snapshot.data!.docs[index];
+                    return RecipeCard(
+                      title: recipe['title'],
+                      imageUrl: const AssetImage('images/pasta.jpg'),
+                      calories:
+                          "${String.fromCharCode(0x00B7)}${recipe['calories']} cal",
+                      cookTime: recipe['cookTime'],
+                      cuisine: recipe['cuisine'],
+                      ingredients: List<String>.from(recipe['ingredients']),
+                      instructions: List<String>.from(recipe['instructions']),
+                    );
+                  },
                 );
               },
             ),
--- lib/new_recipe.dart
+++ lib/new_recipe.dart
@@ -1,144 +1,287 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class NewRecipe extends StatelessWidget {
+class NewRecipe extends StatefulWidget {
   const NewRecipe({Key? key}) : super(key: key);
 
   @override
+  _NewRecipeState createState() => _NewRecipeState();
+}
+
+class _NewRecipeState extends State<NewRecipe> {
+  final _addRecipeFormKey = GlobalKey<FormState>();
+  final _recipeNameController = TextEditingController();
+  final _recipeImageController = TextEditingController();
+  final _caloriesController = TextEditingController();
+  final _cookTimeController = TextEditingController();
+  final _cuisineController = TextEditingController();
+  final _ingredientsController = TextEditingController();
+  final _instructionsController = TextEditingController();
+  @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 16, right: 16, bottom: 16, top: 8),
-      // create a form asking for a anew recipe which user wants to add
-      // form should also ask for image of the recipe
-      child: ListView(
-        children: [
-          const Text(
-            "Add a new recipe",
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-          const SizedBox(height: 16),
-          const TextField(
-            decoration: InputDecoration(
-              labelText: "Recipe name",
-              prefixIcon: Icon(Icons.search),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(16),
-                ),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(16),
-                ),
+      padding: EdgeInsets.only(left: 16, right: 16, bottom: 16, top: 8),
+      child: Form(
+        key: _addRecipeFormKey,
+        child: ListView(
+          children: [
+            const Text(
+              "Add a new recipe",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w500,
               ),
             ),
-          ),
-          const SizedBox(height: 16),
-          const TextField(
-            decoration: InputDecoration(
-              labelText: "Recipe image",
-              prefixIcon: Icon(Icons.image),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(16),
+            const SizedBox(height: 16),
+            TextFormField(
+              controller: _recipeNameController,
+              decoration: const InputDecoration(
+                labelText: "Recipe name",
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(16),
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(16),
+                  ),
                 ),
               ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(16),
-                ),
-              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter a recipe name';
+                }
+                return null;
+              },
             ),
-          ),
-          const SizedBox(height: 16),
-          const TextField(
-            decoration: InputDecoration(
-              labelText: "Calories",
-              prefixIcon: Icon(Icons.local_fire_department),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(16),
+            const SizedBox(height: 16),
+            TextFormField(
+              controller: _recipeImageController,
+              decoration: const InputDecoration(
+                labelText: "Recipe image",
+                prefixIcon: Icon(Icons.image),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(16),
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(16),
+                  ),
                 ),
               ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(16),
-                ),
-              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter a recipe image';
+                }
+                return null;
+              },
             ),
-          ),
-          const SizedBox(height: 16),
-          const TextField(
-            decoration: InputDecoration(
-              labelText: "Cook time",
-              prefixIcon: Icon(Icons.timer),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(16),
+            const SizedBox(height: 16),
+            TextFormField(
+              controller: _caloriesController,
+              decoration: const InputDecoration(
+                labelText: "Calories",
+                prefixIcon: Icon(Icons.local_fire_department),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(16),
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(16),
+                  ),
                 ),
               ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(16),
-                ),
-              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter the calories';
+                }
+                return null;
+              },
             ),
-          ),
-          const SizedBox(height: 16),
-          const TextField(
-            decoration: InputDecoration(
-              labelText: "Cuisine",
-              prefixIcon: Icon(Icons.food_bank),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(16),
+            const SizedBox(height: 16),
+            TextFormField(
+              controller: _cookTimeController,
+              decoration: const InputDecoration(
+                labelText: "Cook time",
+                prefixIcon: Icon(Icons.timer),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(16),
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(16),
+                  ),
                 ),
               ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(16),
-                ),
-              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter the cook time';
+                }
+                return null;
+              },
             ),
-          ),
-          const SizedBox(height: 16),
-          const TextField(
-            decoration: InputDecoration(
-              labelText: "Ingredients",
-              prefixIcon: Icon(Icons.food_bank_outlined),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(16),
+            const SizedBox(height: 16),
+            TextFormField(
+              controller: _cuisineController,
+              decoration: const InputDecoration(
+                labelText: "Cuisine",
+                prefixIcon: Icon(Icons.local_dining),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(16),
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(16),
+                  ),
                 ),
               ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(16),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter the cuisine';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 16),
+            TextFormField(
+              controller: _ingredientsController,
+              minLines: 1,
+              maxLines: 5,
+              decoration: const InputDecoration(
+                labelText: "Ingredients",
+                prefixIcon: Icon(Icons.shopping_bag),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(16),
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(16),
+                  ),
                 ),
               ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter the ingredients';
+                }
+                return null;
+              },
             ),
-          ),
-          const SizedBox(height: 16),
-          const TextField(
-            minLines: 1,
-            maxLines: 5,
-            decoration: InputDecoration(
-              labelText: "Instructions",
-              prefixIcon: Icon(Icons.integration_instructions),
-              border: OutlineInputBorder(),
+            const SizedBox(height: 16),
+            TextFormField(
+              controller: _instructionsController,
+              minLines: 1,
+              maxLines: 5,
+              decoration: const InputDecoration(
+                labelText: "Instructions",
+                prefixIcon: Icon(Icons.list),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(16),
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(16),
+                  ),
+                ),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter the instructions';
+                }
+                return null;
+              },
             ),
-          ),
-          const SizedBox(height: 16),
-          ElevatedButton(
-            onPressed: () {
-              // add the recipe to the database
-            },
-            child: const Text("Add recipe"),
-          ),
-        ],
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                if (_addRecipeFormKey.currentState!.validate()) {
+                  Recipe r = Recipe(
+                    name: _recipeNameController.text,
+                    image: AssetImage(_recipeImageController.text),
+                    calories: _caloriesController.text,
+                    cookTime: _cookTimeController.text,
+                    cuisine: _cuisineController.text,
+                    ingredients: _ingredientsController.text.split(','),
+                    instructions: _instructionsController.text.split(','),
+                  );
+                  var status = r.saveRecipe();
+                  if (status) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text('Recipe added successfully'),
+                      ),
+                    );
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text('Failed to add recipe'),
+                      ),
+                    );
+                  }
+                  _addRecipeFormKey.currentState!.reset();
+                }
+              },
+              child: const Text('Add recipe'),
+            ),
+          ],
+        ),
       ),
     );
+  }
+}
+
+class Recipe {
+  final String name;
+  AssetImage? image;
+  final String calories;
+  final String cookTime;
+  final String cuisine;
+  final List<String> ingredients;
+  final List<String> instructions;
+
+  Recipe({
+    required this.name,
+    required this.calories,
+    required this.cookTime,
+    required this.cuisine,
+    required this.ingredients,
+    required this.instructions,
+    this.image,
+  });
+
+  @override
+  String toString() {
+    final String ingredientsString = ingredients.join(', ');
+    final String instructionsString = instructions.join(', ');
+    return 'Name: $name\nCalories: $calories\nCook time: $cookTime\nCuisine: $cuisine\nIngredients: $ingredientsString\nInstructions: $instructionsString';
+  }
+
+  bool saveRecipe() {
+    try {
+      FirebaseFirestore.instance.collection('recipes').add({
+        'title': name,
+        'calories': calories,
+        'cookTime': cookTime,
+        'cuisine': cuisine,
+        'ingredients': ingredients,
+        'instructions': instructions,
+      });
+      return true;
+    } catch (e) {
+      return false;
+    }
   }
 }
